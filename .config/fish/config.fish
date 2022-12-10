3ef3ff024f66ce6f1ea99cd1e19a8888238450c2
heisenbug <heisenbug@xyz.com>
2022-12-10 17:44:42 +0800

    macintosh: dotfiles

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,8 @@
 if test -z "$TMUX"
     set PATH $HOME/.local/bin $PATH
+
+    set    PATH /usr/local/opt/coreutils/libexec/gnubin    $PATH
+    set MANPATH /usr/local/opt/coreutils/libexec/gnuman $MANPATH
 end
 
 set SHELL (which fish)
@@ -12,4 +15,8 @@ if status is-interactive
     set VISUAL nvim
 
     bind \cw backward-kill-bigword
+
+    # if test -z "$TMUX"
+    #     osascript -e 'tell application "System Events"' -e 'set position of first window of application process "kitty" to {232, 67}' -e 'end tell'
+    # end
 end
